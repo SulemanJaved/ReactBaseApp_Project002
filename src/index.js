09eb1f62f1b4e4ad58599f2ff8541f418e69b0c3
Suleman Javed <sulemanjaved20@gmail.com>
2020-10-23 21:58:21 +0500

    Update index.js and added new code

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div><App name="Suleman" age={30-4} /></div>,
   document.getElementById('root')
 );
 
