b90564680751276f1d2da72ee024f34d007aa0f7
Suleman Javed <sulemanjaved20@gmail.com>
2020-10-23 22:54:01 +0500

    corrected space in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Hello from './Hello';
 
 function App({name, age}) {
   return <div>
-          Hello World from <strong>{name}</strong> having age of{age}
+          Hello World from <strong>{name}</strong> having age of {age}
           <br/>
           <Hello firstName={name}></Hello>
         </div>
